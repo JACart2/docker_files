55f617221765f69896ec778e745c5aff2b43c625
Jackson C. Art Jr <camndon.reed@gmail.com>
2025-03-12 14:25:12 -0400

    updated dependencies for docker file

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ bash ./initialize_host.sh
 open_browser_when_ready () {
 	until curl -s http://localhost:5173 > /dev/null
 	do
-	  echo "Waiting for port 5173 to open."
+	# This is just waiting for the application to start. If the container is not up and running, this will wait forever.
+	#   echo "Waiting for port 5173 to open."
 	  sleep 2
 	done
 	open http://localhost:5173
