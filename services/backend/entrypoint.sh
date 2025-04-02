2eb3332e9a8411a908d240454ecc12973f3d5d27
Jackson C. Art Jr <camndon.reed@gmail.com>
2025-04-02 14:15:24 -0400

    Working version 1

--- services/backend/entrypoint.sh
+++ services/backend/entrypoint.sh
@@ -3,9 +3,25 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/humble/setup.bash" --
+source "/dependency_ws/install/setup.bash"
+cd /dev_ws
+rosdep update
+rosdep install --from-paths /dev_ws/src --ignore-src -r -y 
+colcon build --symlink-install
 source "/dev_ws/install/setup.bash" --
 echo 'source "/opt/ros/humble/setup.bash" ' >> ~/.bashrc 
 echo 'source "/dev_ws/install/setup.bash" ' >> ~/.bashrc 
+
+# Environment settings that need src to be used
+
+PKG="/dev_ws/src/ai-navigation/motor_control"
+
+# sets up a rule file that automatically sets the arduino to port "ttyUSB9" everytime
+cp "${PKG}/resource/99-usb-serial.rules" "/etc/udev/rules.d"
+
+# installs all required packages
+pip install -r ${PKG}/resource/requirements.txt
+
 # take in command line input and execute
 
 if [[ -z "$BACKEND_COMMAND" ]]; then
