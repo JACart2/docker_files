b61a7c117d5fa0ce3873850c2c9c94324c99f36d
Nathan Sprague <nathan.r.sprague@gmail.com>
2026-01-09 11:31:33 -0500

    fixes from cart testing

--- services/backend/entrypoint.sh
+++ services/backend/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 # setup ros2 environment
 source "/opt/ros/jazzy/setup.bash" --
 source "/opt/ros_ws/install/setup.bash" --
-source "/dev_ws/install/setup.bash" --
+#source "/dev_ws/install/setup.bash" --
 echo 'source "/opt/ros/jazzy/setup.bash" ' >> ~/.bashrc 
 echo 'source "/opt/ros_ws/install/setup.bash" ' >> ~/.bashrc 
-echo 'source "/dev_ws/install/setup.bash" ' >> ~/.bashrc 
+#echo 'source "/dev_ws/install/setup.bash" ' >> ~/.bashrc 
 # take in command line input and execute
 
 if [[ -z "$BACKEND_COMMAND" ]]; then
