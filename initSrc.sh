65b8be1f2b41603cf49b16c6af94f4ade37e31d2
Gr8jj28 <gr8jj28@gmail.com>
2026-01-19 18:41:42 -0500

    removing files not needed in main, removing redundant install steps in backend dockerfile

--- initSrc.sh
+++
@@ -1,9 +0,0 @@
-# Run this one time when first creating the dev_ws folder on a new computer
-
-# Get packages -------------------------------------------------------------------------------------------
-echo "Cloning repos"
-mkdir -p src
-cd src
-git clone --branch display_global_path https://github.com/JACart2/ai-navigation.git && \
-git clone --branch display_global_path https://github.com/JACart2/FER_DataCollection.git && \
-	echo "Repos have been set up. This workspace will be build by the docker entry script every time it runs."
