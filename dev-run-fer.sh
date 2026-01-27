6f759cf5446085cbfe45db42370dc94becaf2899
Gr8jj28 <gr8jj28@gmail.com>
2026-01-26 19:56:18 -0500

    overhaul fer dockerfile to match backend conventions

---
+++ dev-run-fer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+bash ./initialize_host.sh
+
+STALL="tail -f /dev/null"
+
+FRONTEND_COMMAND=$STALL BACKEND_COMMAND=$STALL FER_COMMAND=$STALL docker compose up --build --remove-orphans --force-recreate -d
+
+# Launch VS Code attached to the container
+if command -v code &> /dev/null; then
+    CONTAINER_ID=$(docker compose ps -q fer)
+    if [ -n "$CONTAINER_ID" ]; then
+        CONTAINER_NAME=$(docker inspect --format '{{.Name}}' $CONTAINER_ID | sed 's/^\///')
+        if [ -n "$CONTAINER_NAME" ]; then
+             HEX_NAME=$(printf "$CONTAINER_NAME" | od -A n -t x1 | tr -d ' \n')
+             URI="vscode-remote://attached-container+${HEX_NAME}/dev_ws"
+             echo "Opening VS Code attached to ${CONTAINER_NAME}..."
+             code --folder-uri "$URI"
+        fi
+    fi
+fi
+
+# Attach a terminal to the backend
+docker compose exec -it -w /dev_ws fer bash -c 'source /opt/ros/jazzy/setup.bash && source /opt/ros_ws/install/setup.bash && ([ -f /dev_ws/install/setup.bash ] && source /dev_ws/install/setup.bash); exec bash'
+
+
--- services/fer/entrypoint.sh
+++ services/fer/entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 export OA_SECRET=$(cat /mnt/OA_SECRET.txt)
 
-echo "OA_SECRET is set to: $OA_SECRET"
+echo "OA_SECRET loaded (length: ${#OA_SECRET})"
 
 # setup ros2 environment
-source "/opt/ros/humble/setup.bash" --
-source "/dev_ws/install/setup.bash" --
-echo 'source "/opt/ros/humble/setup.bash" ' >> ~/.bashrc 
-echo 'source "/dev_ws/install/setup.bash" ' >> ~/.bashrc 
-
-# take in command line input and execute
+source "/opt/ros/jazzy/setup.bash" 
+source "/dev_ws/install/setup.bash" 
 
 # if no FER_COMMAND, default to compose.yml CMD. Otherwise, use FER_COMMAND
 if [[ -z "$FER_COMMAND" ]]; then
   exec "$@"
 else
-  bash -c "$FER_COMMAND"
+  exec bash -lc "$FER_COMMAND"
 fi
 
