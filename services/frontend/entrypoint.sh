5cf5914948927a4f6d64a009d4d36812f5cfaf03
Jake Dinh <138043055+Qwertoses@users.noreply.github.com>
2026-03-12 12:29:21 -0400

    Update entrypoint.sh to use UI_DIR variable

--- services/frontend/entrypoint.sh
+++ services/frontend/entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+UI_DIR="/root/dev_ws/src/ui"
+
 if [[ -z "$FRONTEND_COMMAND" ]]; then
   exec "$@"
 else
-  # Update UI code from GitHub (if /ui is a git repo)
-  if [ -d "/ui/.git" ]; then
+  # Update UI code from GitHub (if ui is a git repo)
+  if [ -d "$UI_DIR/.git" ]; then
     echo "Updating UI from GitHub..."
-    cd /ui
+    cd "$UI_DIR"
     git pull origin main || echo "Warning: Git pull failed, using existing code."
     npm install || echo "Warning: npm install failed."
   else
-    echo "Error: /ui is not a Git repository. Cannot update UI."
+    echo "Error: $UI_DIR is not a Git repository. Cannot update UI."
   fi
 
   # Execute the frontend command
   bash -c "$FRONTEND_COMMAND"
 fi
-
