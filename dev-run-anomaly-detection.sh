b56fb7d98ec8e7a2c38b44c4caa660ae4185b024
john <gr8jj28@gmail.cmo>
2026-04-10 16:08:50 -0400

    update aad dev-run for frontend compatibility

--- dev-run-anomaly-detection.sh
+++ dev-run-anomaly-detection.sh
@@ -2,9 +2,20 @@
 
 bash ./initialize_host.sh
 
+
+open_browser_when_ready () {
+	until curl -s http://localhost:5173 > /dev/null
+	do
+	# This is just waiting for the application to start. If the container is not up and running, this will wait forever.
+	#   echo "Waiting for port 5173 to open."
+	  sleep 2
+	done
+	open http://localhost:5173
+}
+
 STALL="tail -f /dev/null"
 
-FRONTEND_COMMAND=$STALL ANOMALY_DETECTION_COMMAND=$STALL docker compose up --build --remove-orphans --force-recreate 
+ANOMALY_DETECTION_COMMAND=$STALL docker compose up --build --remove-orphans --force-recreate 
 
 # Launch VS Code attached to the container
 if command -v code &> /dev/null; then
@@ -13,7 +24,7 @@ if command -v code &> /dev/null; then
         CONTAINER_NAME=$(docker inspect --format '{{.Name}}' $CONTAINER_ID | sed 's/^\///')
         if [ -n "$CONTAINER_NAME" ]; then
              HEX_NAME=$(printf "$CONTAINER_NAME" | od -A n -t x1 | tr -d ' \n')
-             URI="vscode-remote://attached-container+${HEX_NAME}/dev_ws"
+             URI="vscode-remote://attached-container+${HEX_NAME}/root/dev_ws"
              echo "Opening VS Code attached to ${CONTAINER_NAME}..."
              code --folder-uri "$URI"
         fi
@@ -21,7 +32,7 @@ if command -v code &> /dev/null; then
 fi
 
 # Attach a terminal to the anomaly_detection
-docker compose exec -it -w /root/dev_ws anomaly_detection bash -c 'source /opt/ros/jazzy/setup.bash && source /opt/ros_ws/install/setup.bash && ([ -f /root/dev_ws/install/setup.bash ] && source /root/dev_ws/install/setup.bash); exec bash'
+open_browser_when_ready & docker compose exec -it -w /root/dev_ws anomaly_detection bash -c 'source /opt/ros/jazzy/setup.bash && source /opt/ros_ws/install/setup.bash && ([ -f /root/dev_ws/install/setup.bash ] && source /root/dev_ws/install/setup.bash); exec bash'
 
 
 
