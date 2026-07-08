8a6678526706abea5b8796e50089aab1c1d10e7e
Hunter Fauntleroy <fauntlht@dukes.jmu.edu>
2026-07-08 11:58:02 -0400

    removed unneeded code

--- services/anomaly_detection/entrypoint.sh
+++ services/anomaly_detection/entrypoint.sh
@@ -6,16 +6,6 @@ fi
 if [ -f "/root/dev_ws/install/setup.bash" ]; then
     source "/root/dev_ws/install/setup.bash" --
 fi
-grep -qxF 'source "/opt/ros/jazzy/setup.bash"' ~/.bashrc || \
-    echo 'source "/opt/ros/jazzy/setup.bash"' >> ~/.bashrc
-grep -qxF 'if [ -f "/opt/ros_ws/install/setup.bash" ]; then source "/opt/ros_ws/install/setup.bash"; fi' ~/.bashrc || \
-    echo 'if [ -f "/opt/ros_ws/install/setup.bash" ]; then source "/opt/ros_ws/install/setup.bash"; fi' >> ~/.bashrc
-grep -qxF 'if [ -f "/root/dev_ws/install/setup.bash" ]; then source "/root/dev_ws/install/setup.bash"; fi' ~/.bashrc || \
-    echo 'if [ -f "/root/dev_ws/install/setup.bash" ]; then source "/root/dev_ws/install/setup.bash"; fi' >> ~/.bashrc
-
-if [ -e "/root/dev_ws/src/anomaly_detection/COLCON_IGNORE" ]; then
-    rm -f "/root/dev_ws/src/anomaly_detection/COLCON_IGNORE"
-fi
 
 
 # if no ANOMALY_DETECTION_COMMAND, default to compose.yml CMD. Otherwise, use ANOMALY_DETECTION_COMMAND
